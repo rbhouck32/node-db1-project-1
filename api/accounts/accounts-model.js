9bace0f5e789e81b1751233c6e29e4c61bbb3cc4
Rob <rbhouck32@gmail.com>
2021-01-19 11:44:50 -0500

    Wrote endpoints for the getbyID and post methods

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,4 +1,4 @@
-const db = require("../../data/db-config");
+const db = require("../../data/dbConfig.js");
 
 module.exports = {
   get,
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -21,4 +21,22 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const accountData = await Account.getById(req.params.id);
+    res.status(200).json(accountData);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newAccount = await Account.add(req.body);
+    res.status(201).json(newAccount);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
